ef3006b120ab89daaaa73d795a7c8b446991d16f
Diego <diego.suarez@usach.cl>
2019-01-18 02:08:10 -0300

    arreglos en home
    quite algunos botones por defecto en mapa
    arreglos en navbar
    botones

--- js/mapa.js
+++ js/mapa.js
@@ -4,7 +4,14 @@ function initMap() {
     var uluru = {lat: -33.449725877092554, lng: -70.68698197603226};
     // The map, centered at Uluru
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 19, center: uluru});
+        document.getElementById('map'), {zoom: 19, center: uluru,
+        // Para que no aparezcan algunos botones por defecto:
+        zoomControl: false,
+        mapTypeControl: true,
+        scaleControl: true,
+        streetViewControl: true,
+        rotateControl: true,
+        fullscreenControl: false});
     // The marker, positioned at Uluru
     var marker = new google.maps.Marker({position: uluru, map: map});
     map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
